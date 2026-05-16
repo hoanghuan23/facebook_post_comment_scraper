e1a62e85a441ee2d47a9a8c0f86a58017c9e7759
hoanghuan23 <huanhoang2003@gmail.com>
2026-05-16 09:30:22 +0700

    cấu hình job_default cho phép nếu bị trễ dưới 10p thì đc chạy bù để tránh bị miss

--- backend/scheduler/task_scheduler.py
+++ backend/scheduler/task_scheduler.py
@@ -22,7 +22,13 @@ async def start_scheduler():
         health_check,
     )
     
-    scheduler = AsyncIOScheduler()
+    scheduler = AsyncIOScheduler(
+        job_defaults={
+            'coalesce': True,  # Nếu một job bị trễ, chỉ chạy lần
+            "max_instances": 1,  # Chỉ cho phép một instance của job chạy cùng lúc
+            "misfire_grace_time": 600,  # Nếu job bị trễ hơn 10 phút, bỏ qua lần chạy đó
+        }
+    )
     
     if settings.SCHEDULER_ENABLED:
         # Add scheduled tasks
